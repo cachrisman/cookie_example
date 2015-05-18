cf34564dc2ac194f2acf21879b2bb8156974a32b
cachrisman <charlie.chrisman@gmail.com>
2015-05-18 10:53:51 -0700

    using cookie-parser

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-var express = require("express");
+var express = require('express');
+var cookieParser = require('cookie-parser');
 
 var app = express();
+app.use(cookieParser());
 
 app.get("/", function(req, res) {
-    console.log(req.headers);
-    var cookieStr = req.get("Cookie");
-    var count = 0;
-    if (cookieStr) {
-        count = parseInt(cookieStr.split("=")[1]);
-    }
-    count += 1;
+    console.log(req.cookies);
+    var count = parseInt(req.cookies.count);
+    count++;
     res.cookie('count', count);
     res.send("Hello World");
 });
